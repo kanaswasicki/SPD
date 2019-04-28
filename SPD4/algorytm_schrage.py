a245d1cc68bf58f0351645f6a30f6cdcedcfdc75
Milosz Kanas <milosz.kanas@gmail.com>
2019-04-28 15:06:25 +0200

    Schrage i schrage z podzialem sprawdzone ze poprawne

--- SPD4/algorytm_schrage.py
+++ SPD4/algorytm_schrage.py
@@ -30,38 +30,62 @@ def schrage(N):
     NG = []
     NN = copy.deepcopy(N)
     t = min(NN)[0]
+    Cmax = 0
     while (NN != [] or NG != []):
         while (NN != [] and t >= min(NN)[0]):
-            j = [NN.index(min(NN))]
-            j.extend(NN[j[0]])
-            NG.append(j)
-            NN.pop(j[0])
+            j = NN.index(min(NN))
+            NG.append(NN[j])
+            NN.pop(j)
 
         if NG == []:
             t = min(NN)[0]
         else:
-            j = NG.index(max(NG, key=operator.itemgetter(3)))
+            i = NG.index(max(NG, key=operator.itemgetter(2)))
+            j = NG[i]
+            NG.pop(i)
+            teta.append(j)
+            t = t + j[1]
+            Cmax = max(Cmax, t+j[2])
+    return teta, Cmax
 
-            teta.append(NG[j][0])
-            t = t + NG[j][2]
-            NG.pop(j)
-    return teta
 
-
-def licz_Cmax(kolejnosc, tabela):
-    r = 0
-    p = 0
+def schragepmtn(N):
+    teta = []
+    NG = []
+    NN = copy.deepcopy(N)
+    t = min(NN)[0]
     Cmax = 0
-    for a in kolejnosc:
-        r += tabela[a][0]
-        p = max(p, r) + tabela[a][1]
-        Cmax = max(Cmax, p+tabela[a][2])
-    return Cmax
+    l = [0, 0, 100000000]
+    while (NN != [] or NG != []):
+        while (NN != [] and t >= min(NN)[0]):
+            i = NN.index(min(NN))
+            j = NN[i]
+            NG.append(j)
+            NN.pop(i)
+            if j[2] > l[2]:
+                l[1] = t - j[0]
+                t = j[0]
+                if l[1] > 0:
+                    NG.append(l)
+        if NG == []:
+            t = min(NN)[0]
+        else:
+            i = NG.index(max(NG, key=operator.itemgetter(2)))
+            j = NG[i]
+            NG.pop(i)
+            teta.append(j)
+            l = j
+            t = t + j[1]
+            Cmax = max(Cmax, t+j[2])
+    return teta, Cmax
 
 
-plik = "SPD4\\in200.txt"
-tabela = przygotowanie_danych(plik)
-kolejnosc = schrage(tabela)
-print(kolejnosc)
-Cmax = licz_Cmax(kolejnosc, tabela)
-print(Cmax)
+a = []
+instancje = [50, 100, 200]
+for i in instancje:
+    plik = "SPD4\\in" + str(i)+".txt"
+    tabela = przygotowanie_danych(plik)
+    kolejnosc, Cmax = schragepmtn(tabela)
+    a.append(Cmax)
+    print(Cmax)
+print(sum(a))
