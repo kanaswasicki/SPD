efb677c24f174f862d0c2a285fe9e897bb104bf6
Milosz Kanas <milosz.kanas@gmail.com>
2019-05-18 20:02:57 +0200

    asd

--- SPD5/Carlier.py
+++ SPD5/Carlier.py
@@ -1,5 +1,6 @@
 import operator
 import copy
+from prettytable import PrettyTable
 
 
 def odczyt(nazwa):
@@ -29,56 +30,63 @@ def przygotowanie_danych(nazwa):
 def schrage(N):
     teta = []
     NG = []
-    NN = copy.deepcopy(N)
-    t = min(NN)[0]
+    NN = []
+    a = 0
+    for i in N:
+        NN.append([a, i[0], i[1], i[2]])
+        a += 1
+    t = min(NN, key=operator.itemgetter(1))[1]
     Cmax = 0
     while (NN != [] or NG != []):
-        while (NN != [] and t >= min(NN)[0]):
-            j = NN.index(min(NN))
+        while (NN != [] and t >= min(NN, key=operator.itemgetter(1))[1]):
+            j = NN.index(min(NN, key=operator.itemgetter(1)))
             NG.append(NN[j])
             NN.pop(j)
 
         if NG == []:
-            t = min(NN)[0]
+            t = min(NN, key=operator.itemgetter(1))[1]
         else:
-            i = NG.index(max(NG, key=operator.itemgetter(2)))
+            i = NG.index(max(NG, key=operator.itemgetter(3)))
             j = NG[i]
             NG.pop(i)
-            teta.append(N.index(j))
-            t = t + j[1]
-            Cmax = max(Cmax, t+j[2])
+            teta.append(j[0])
+            t = t + j[2]
+            Cmax = max(Cmax, t+j[3])
     return teta, Cmax
 
 
 def schragepmtn(N):
-    teta = []
     NG = []
-    NN = copy.deepcopy(N)
-    t = min(NN)[0]
+    NN = []
+    a = 0
+    for i in N:
+        NN.append([a, i[0], i[1], i[2]])
+        a += 1
+    t = min(NN, key=operator.itemgetter(1))[1]
     Cmax = 0
-    l = [0, 0, 100000000]
+    l = [0, 0, 0, 100000000]
     while (NN != [] or NG != []):
-        while (NN != [] and t >= min(NN)[0]):
-            i = NN.index(min(NN))
-            j = NN[i]
-            NG.append(j)
-            NN.pop(i)
-            if j[2] > l[2]:
-                l[1] = t - j[0]
-                t = j[0]
-                if l[1] > 0:
+        while (NN != [] and t >= min(NN, key=operator.itemgetter(1))[1]):
+            j = NN.index(min(NN, key=operator.itemgetter(1)))
+            i = NN[j]
+            NG.append(NN[j])
+            NN.pop(j)
+            if i[3] > l[3]:
+                l[2] = t - i[1]
+                t = i[1]
+                if l[2] > 0:
                     NG.append(l)
         if NG == []:
-            t = min(NN)[0]
+            t = min(NN, key=operator.itemgetter(1))[1]
         else:
-            i = NG.index(max(NG, key=operator.itemgetter(2)))
+            i = NG.index(max(NG, key=operator.itemgetter(3)))
             j = NG[i]
             NG.pop(i)
-            teta.append(j)
             l = j
-            t = t + j[1]
-            Cmax = max(Cmax, t+j[2])
+            t = t + j[2]
+            Cmax = max(Cmax, t+j[3])
     return Cmax
+    #
 
 
 def licz_b(U, n, tabela):
@@ -119,11 +127,13 @@ def licz_c(n, tabela, a, b):
 
 # wszystko teraz pracuje na zmiennych globalnych, zmieniłem sposob liczenia K
 # wyswietla sie tez poziom i kierunek
-def Carlier(n, tabela,wejscie, poziom):
+
+
+def Carlier(n, tabela, wejscie, poziom):
     global best_pi
     global UB
     pi, U = schrage(tabela)
-    print(wejscie, poziom, "\n")
+    #print(wejscie, poziom, "\n")
     if U < UB:
         UB = U
         best_pi = pi
@@ -148,33 +158,39 @@ def Carlier(n, tabela,wejscie, poziom):
     # LEWA
 
     tempR = tabela[c][0]
+    LBL = schragepmtn(tabela)
     tabela[c][0] = max(tabela[c][0], rK+pK)
     temp = tabela[c]
-    LB = schragepmtn(tabela)
+    #LBL = schragepmtn(tabela)
     hkc = min(rK, tabela[c][0])+pK+tabela[c][1]+min(qK, tabela[c][2])
-    LB = max(hK, LB, hkc)
-    if LB < UB:
-        Carlier(best_pi, tabela, 'lewo',poziom+1)
+    LBL1 = max(hK, LBL, hkc)
+    if LBL1 < UB:
+        Carlier(best_pi, tabela, 'lewo', poziom+1)
     tabela[c][0] = tempR
 
     # PRAWA
 
     tempQ = tabela[c][2]
+    LBP = schragepmtn(tabela)
     tabela[c][2] = max(tabela[c][2], qK + pK)
     temp1 = tabela[c]
     hkc = min(rK, tabela[c][0]) + pK + tabela[c][1] + min(qK, tabela[c][2])
-    LB = schragepmtn(tabela)
-    LB = max(hK, hkc, LB)
-    if LB < UB:
+    #LBP = schragepmtn(tabela)
+    LBP1 = max(hK, hkc, LBP)
+    if LBP1 < UB:
         Carlier(best_pi, tabela, 'prawo', poziom+1)
     tabela[c][2] = tempQ
 
 
-
-plik = "data3.txt"
-tabela, n = przygotowanie_danych(plik)
-UB = 99999999
-Carlier(n, tabela, 'srodek', 0)
-print(UB, tabela)
-best_pi = [x+1 for x in best_pi]
-print(best_pi)
+x = PrettyTable()
+x.field_names = ["Carlier", "Permutacja", "Cmax"]
+for i in range(0, 9):
+    plik = 'data'+str(i)+'.txt'
+    tabela, n = przygotowanie_danych(plik)
+    UB = 99999999
+    Carlier(n, tabela, 'srodek', 0)
+    print(UB, '\n')
+    best_pi = [x+1 for x in best_pi]
+    print(best_pi, '\n')
+    x.add_row([plik, best_pi, UB])
+print(x)
